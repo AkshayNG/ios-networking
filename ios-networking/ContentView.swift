63492b5c133a0b936805fcc1903730fd35bf2bac
Akshay Gajarlawar <akshaygajarlawar@gmail.com>
2021-05-09 23:30:46 +0530

    created SwiftUI project

---
+++ ios-networking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-networking
+//
+//  Created by Akshay Gajarlawar on 09/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-networking/ios_networkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_networkingApp.swift
+//  ios-networking
+//
+//  Created by Akshay Gajarlawar on 09/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_networkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
